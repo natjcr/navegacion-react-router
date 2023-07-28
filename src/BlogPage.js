3753931af0cd027fa89e465345dc2fcd0e8a998e
njcre <njcollazosr@gmaile.com>
2023-07-28 12:25:45 -0500

    new1

---
+++ src/BlogPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function BlogPage() {
+    return(
+        <p>BlogPage</p>
+    )
+}
+
+export { BlogPage }
---
+++ src/Menu.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Menu() {
+    return(
+        <nav>
+            <ul>
+                {routes.map(route => (
+                    <li>
+                        <NavLink style={({ isActive }) => ({ color: isActive ? 'red' : 'blue', })}
+                        to={route.to}>
+                            {route.text}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+const routes = [];
+routes.push({
+    to: '/',
+    text: 'Home',
+})
+routes.push({
+    to: '/blog',
+    text: 'Blog',
+})
+routes.push({
+    to: '/profile',
+    text: 'Profile',
+})
+
+
+export { Menu }
